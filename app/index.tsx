e01de6dd8d443c7e1a5eb534c27af6284370c725
Akshita Thakur <akshita.thakur@technossus.com>
2024-12-26 10:50:51 +0530

    circle polygon added

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,48 @@
-import React, { useEffect, useState , useRef } from "react";
-import MapView, { Marker, PROVIDER_GOOGLE, Region , Polyline} from "react-native-maps";
-import { StyleSheet, View, Button, Text } from "react-native";
+import React, { useEffect, useState, useRef } from "react";
+import MapView, {
+  Marker,
+  PROVIDER_GOOGLE,
+  Region,
+  Polyline,
+  Circle,
+  Polygon,
+} from "react-native-maps";
+import { StyleSheet, View, Button, Text, Switch } from "react-native";
 import * as Location from "expo-location";
-import { SanJoseRegion , Stanford , PaloAlto } from "@/constant/Region";
+import {
+  SanJoseRegion,
+  Stanford,
+  PaloAlto,
+  polygonCoordinates,
+} from "@/constant/Region";
 
 export default function App() {
   const mapRef = useRef<MapView>(null);
-
+  const [showMarkers, setShowMarkers] = useState(false);
+  const [markers, setMarkers] = useState<{ latitude: number; longitude: number }[]>([]);
   const [region, setRegion] = useState<Region | null>(null);
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
 
-const gotoSanjose = () =>{
-mapRef.current?.animateToRegion(SanJoseRegion , 3*1000)     //Animate the user to new region.  takes 2 parameter , region and time to Complete this animation 
+  const toggleSwitch = () => {
+    setShowMarkers((previousState) => {
+      const newState = !previousState;
+      if (!newState) {
+        setMarkers([]); // If  i turned off switch, clear markers
+      }
+      return newState;
+    });
+  };
 
-}
+  const gotoSanjose = () => {
+    mapRef.current?.animateToRegion(SanJoseRegion, 3 * 1000); //Animate the user to new region.  takes 2 parameter , region and time to Complete this animation
+  };
 
-  const fetchUserLocation = async () => {
+  const fetchUserLocation = async () => {  //Function to fetc userLocation initially
     let { status } = await Location.requestForegroundPermissionsAsync();
     if (status !== "granted") {
       setErrorMsg("Permission to access location was denied");
       return;
     }
-
     try {
       let location = await Location.getCurrentPositionAsync({
         accuracy: Location.Accuracy.High,
@@ -43,35 +64,87 @@ mapRef.current?.animateToRegion(SanJoseRegion , 3*1000)     //Animate the user t
     }
   };
 
+  const handleUserPress = (event:any) =>{
+    const newPoint = event.nativeEvent.coordinate;
+    setMarkers((prevMarkers) => [...prevMarkers, newPoint]); //Add new point to markers array
+  }
   useEffect(() => {
     fetchUserLocation();
   }, []);
 
   return (
     <View style={styles.container}>
+      <View style={{ flexDirection: "row", gap: 10 }}>
+        <Text style={{ marginTop: 15 }}>Turn on to point merkers on map</Text>
+        <Switch
+          trackColor={{ false: "#767577", true: "#81b0ff" }}
+          onValueChange={toggleSwitch}
+          value={showMarkers}
+        />
+      </View>
+
       {region ? (
         <MapView
-        ref={mapRef}
+          ref={mapRef}
           region={region}
           style={styles.map}
-          onRegionChangeComplete={(region) => setRegion(region)}
-          showsMyLocationButton
           showsUserLocation
-        >
-          <Polyline coordinates={[Stanford , PaloAlto]} strokeColor={"#000"} strokeWidth={3}/>   {/* Create a line between Stanford and PaloAlto */}
-          <Marker   //Chnge the color of the marker
+          onPress={showMarkers ? handleUserPress : undefined} 
+          >
+          { showMarkers && markers.map((points , index)=>(
+            <Marker 
+              key={index}
+              coordinate={points}
+              title={`Point ${index+1}`}
+            />
+          ))}
+          {/* Chnge the color of the marker */}
+          <Marker
+            pinColor="yellow"
+            coordinate={{
+              latitude: region.latitude,
+              longitude: region.longitude,
+            }}
+            title="My Location"
+          />
+          <Polyline
+            coordinates={[Stanford, PaloAlto]}
+            strokeColor={"#000"}
+            strokeWidth={3}
+          />
+          {/* Create a line between Stanford and PaloAlto */}
+          <Marker //Chnge the color of the marker
             pinColor="green"
             coordinate={{
               latitude: 37.36787931793023,
               longitude: -122.03485848886174,
             }}
+            title="My First marker"
+            description="This is just to test this description"
           />
-          <Marker    //Added image in the marker
+          <Marker //Added image in the marker
             image={require("../assets/images/hotels.png")}
             coordinate={{
               latitude: 37.35372687747386,
               longitude: -121.9579436257094,
             }}
+            title="This is a hotel"
+          />
+          <Polygon
+            coordinates={polygonCoordinates} // Added Polygon Component
+            strokeWidth={2}
+            strokeColor="blue"
+            fillColor="rgba(0, 255, 0, 0.3)"
+          />
+          <Circle              //Added circle component
+            center={{
+              latitude: 37.36787931793023,
+              longitude: -122.03485848886174,
+            }}
+            radius={1000} // Radius in meters
+            strokeWidth={2}
+            strokeColor="red"
+            fillColor="rgba(255, 0, 0, 0.3)"
           />
         </MapView>
       ) : (
@@ -79,7 +152,7 @@ mapRef.current?.animateToRegion(SanJoseRegion , 3*1000)     //Animate the user t
       )}
       {errorMsg && <Text>{errorMsg}</Text>}
 
-      <Button onPress={()=> gotoSanjose()} title="Go To SanJose"/>
+      <Button onPress={() => gotoSanjose()} title="Go To SanJose" />
     </View>
   );
 }
--- constant/Region.ts
+++ constant/Region.ts
@@ -18,4 +18,12 @@
     latitudeDelta: 0.01,
     longitudeDelta: 0.01,
   }
+
+  export const polygonCoordinates = [
+    { latitude: 37.38457520400125, longitude: -122.11229362081772 },   //Los Altos
+    { latitude: 37.39180396906615, longitude: -122.08087959006708 }, //Mountain view
+    { latitude: 37.44007032332006, longitude: -122.13775736882842 },  
+    { latitude: 37.40868856019358, longitude: -122.17917365068881 }, 
+  ]; // <-- Added polygon coordinates 
+
   
